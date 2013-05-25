e38548657481831278fd70c29821454484aab55b
Kaos Dynamics <mpreziuso@kaosdynamics.com>
2013-05-25 15:37:30 +0300

    Renamed class and namespace (PSR-0 compliant)

---
+++ Exception/OAuthException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace \Twitter\Exception
+
+class OAuthException extends \Exception
+{
+  public static function raise($response, $debug)
+  {
+    $message = $response->responseText;
+
+    switch($response->code)
+    {
+      case 400:
+        throw new BadRequest($message, $response->code);
+      case 401:
+        throw new Unauthorized($message, $response->code);
+      default:
+        throw new OAuthException($message, $response->code);
+    }
+  }
+}
+
+
+?>
