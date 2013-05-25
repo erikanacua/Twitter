ec9bf232a833f887e0ccbeb86cbcc60fbec7609b
Kaos Dynamics <mpreziuso@kaosdynamics.com>
2013-05-25 15:16:40 +0300

    Create EpiOAuthException.php

---
+++ EpiOAuthException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace \Base\Twitter;
+
+class EpiOAuthException extends \Exception
+{
+  public static function raise($response, $debug)
+  {
+    $message = $response->responseText;
+
+    switch($response->code)
+    {
+      case 400:
+        throw new EpiOAuthBadRequestException($message, $response->code);
+      case 401:
+        throw new EpiOAuthUnauthorizedException($message, $response->code);
+      default:
+        throw new EpiOAuthException($message, $response->code);
+    }
+  }
+}
+
+
+?>
