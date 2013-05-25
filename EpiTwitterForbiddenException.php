ced9e8a4af1114cacfafe6ffb034b26245c6a73b
Kaos Dynamics <mpreziuso@kaosdynamics.com>
2013-05-25 14:47:16 +0200

    Renamed class and namespace (PSR-0 compliant)

--- EpiTwitterForbiddenException.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace \Base\Twitter;
-class EpiTwitterForbiddenException extends EpiTwitterException{}
-
-?>
---
+++ Exception/TwitterForbidden.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace \Twitter\Exception;
+class TwitterForbidden extends \Twitter\Exception\TwitterException{}
+
+?>
